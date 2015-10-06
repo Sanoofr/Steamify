f0d92a0fb342a4f055eecb926a541e2fe4f02db5
Sebastien Le Mouillour <dev.n2ofr@gmail.com>
2015-10-06 12:32:53 +0200

    DRAFT : récupération info steam profile

--- api/controllers/SteamController.js
+++ api/controllers/SteamController.js
@@ -9,16 +9,37 @@ module.exports = {
     /**
      * `SteamController.home()`
      */
-    home: function (req, res) {
+    home: function (req, res, options) {
+        var steam = require('steam-web');
+
+        var s = new steam({
+            apiKey: '',
+            format: 'json' //optional ['json', 'xml', 'vdf']
+        });
         Passport
             .findOne({user: req.user.id})
             .exec(function (err, passport) {
+                var user = passport.identifier;
+
+
+                console.log(user);
+                var identif = user.split("/");
+                s.getPlayerSummaries({
+                    steamids: [identif[5]],
+                    callback: function (err, data) {
+                        console.log(JSON.stringify(data));
+                        //return res.json({data: data});
+                    }
+                })
                 return res.json({
                     todo: 'home() is not implemented yet! But you are logged in :)',
+                    user: req.user,
                     token: passport.tokens === undefined ? passport.accessToken : passport.tokens.token
                     //steam don't stock token in bdd
                 });
+
             });
+
     },
     /**
      * `SteamController.remotehome()`
@@ -29,4 +50,5 @@ module.exports = {
             todo: 'remotehome() is not implemented yet! But you are authorized :)'
         });
     }
-};
+}
+;
